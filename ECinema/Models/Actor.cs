7cd357e26ef9e185b0fb4073ac87c9cfff33e9f2
Dawit Teklu <se.dawit.teklu@gmail.com>
2020-12-15 16:36:31 +0300

    Adding Modles

---
+++ ECinema/Models/Actor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ECinema.Models
+{
+    public class Actor
+    { 
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Profile Picture")]
+        [Required(ErrorMessage ="Profile picture url is required")]
+        public string ProfilePictureURL { get; set; }
+        [DisplayName("Full name")]
+        [Required(ErrorMessage ="Full name is required")]
+        [StringLength(50,MinimumLength = 3,ErrorMessage ="Full name must be between 3 and 50 characters")]
+        public string FullName { get; set; }
+        [Required(ErrorMessage ="little bio is required")]
+        [StringLength(150,MinimumLength = 10,ErrorMessage ="Bio must be between 10 and 150 characters")]
+        public string Bio { get; set; }
+
+        public List<Actor_Movie> Movies { get; set; }
+    }
+}
---
+++ ECinema/Models/Producer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECinema.Models
+{
+    public class Producer
+    {
+        [Key]
+        public int ProducerID { get; set; }
+        [DisplayName("Profile Picture")]
+        public string ProfilePictureURL { get; set; }
+        [DisplayName("Full name")]
+        public string FullName { get; set; }
+        public string Bio { get; set; }
+        public List<Movie> Movies { get; set; }
+
+    }
+}
