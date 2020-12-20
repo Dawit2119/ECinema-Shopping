dd2d1937f451b6b12a5e39456d498a3e3d0eaea6
Dawit Teklu <se.dawit.teklu@gmail.com>
2020-12-20 23:38:24 +0300

    Adding the movies and Cinemas controllers

---
+++ ECinema/Controllers/CinemaController.cs
@@ -0,0 +1,24 @@
+﻿using ECinema.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECinema.Controllers
+{
+    public class CinemaController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CinemaController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var data = await _context.Cinemas.ToListAsync();
+            return View(data);
+        }
+    }
+}
---
+++ ECinema/Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using ECinema.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECinema.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly AppDbContext _context;
+        public MovieController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var data = await _context.Movies.Include(m=>m.Cinema).OrderByDescending(m=>m.EndDate).ToListAsync();
+            return View(data);
+        }
+    }
+}
